25c736f112637927a5b1e6a3bc9a1b2f456e5d40
George Vorobiov <george@oyenetwork.com>
2024-11-29 12:45:07 +0100

    feat: add favorite page & service

--- src/app/pages/favorites-grid/favorites-grid.component.ts
+++ src/app/pages/favorites-grid/favorites-grid.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+
+import { Photo, PhotosGridItemComponent } from '../../../shared';
+import { FavoriteService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-favorites-grid',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, PhotosGridItemComponent],
   templateUrl: './favorites-grid.component.html',
-  styleUrl: './favorites-grid.component.scss'
+  styleUrl: './favorites-grid.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FavoritesGridComponent {
+  readonly service = inject(FavoriteService);
+  readonly router = inject(Router);
 
+  photos = signal(this.service.getFavoritePhotos());
+
+  openDetails(photo: Photo): void {
+    this.router.navigate(['/photos', photo.id]);
+  }
 }
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,9 +1,59 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
+
+import { Photo } from '../../shared';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class FavoritesService {
+export class FavoriteService {
+  private readonly cookieService = inject(CookieService);
+  private readonly toastr = inject(ToastrService);
 
-  constructor() { }
+  private cookieKey = 'favorite_photos';
+
+  getFavoritePhotos(): Photo[] {
+    const cookieData = this.cookieService.get(this.cookieKey);
+    return cookieData ? JSON.parse(cookieData) : [];
+  }
+
+  getFavoritePhotoById(id: string): Photo | null {
+    const photos = this.getFavoritePhotos();
+    const foundPhoto = photos.find((p) => p.id === id);
+
+    return foundPhoto ?? null;
+  }
+
+  addPhotoItem(photo: Photo): void {
+    const favorites = this.getFavoritePhotos();
+    const isPresent = favorites.find((p) => p.id === photo.id);
+
+    if (!isPresent) {
+      const { download_url, author, id } = photo;
+
+      favorites.push({ download_url, author, id } as Photo);
+      this.saveToCookies(favorites);
+      this.toastr.success('Photo has been added to favorites', 'Successful!');
+    } else {
+      this.toastr.info('This photo already exist in favorites', 'Information!');
+    }
+  }
+
+  removePhotoItem(photoId: string): void {
+    const favorites = this.getFavoritePhotos();
+    const updatedFavorites = favorites.filter((photo) => photo.id !== photoId);
+
+    this.saveToCookies(updatedFavorites);
+
+    this.toastr.success('Photo has been deleted from favorites', 'Successful!');
+  }
+
+  private saveToCookies(favorites: Photo[]): void {
+    this.cookieService.set(this.cookieKey, JSON.stringify(favorites), {
+      expires: 7, // Expires in 7 days
+      path: '/',
+    });
+  }
 }
